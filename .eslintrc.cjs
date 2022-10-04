5ee333a086abaa57bd09588298568256c90e916d
Tim Whitbeck <tim.whitbeck@stord.com>
2022-10-04 13:27:26 -0400

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "prettier",
+  ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+export default function App() {
+  return (
+    <>
+      <header className="bg-emerald-50 p-6 shadow shadow-emerald-800/50">
+        <h1 className="text-2xl text-emerald-700">Coupon Collector</h1>
+      </header>
+
+      <main className="m-6">
+        <p>This is the stuff</p>
+      </main>
+    </>
+  );
+}
